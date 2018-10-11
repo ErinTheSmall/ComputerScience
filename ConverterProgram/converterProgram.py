bf4c7a8ba60b7420f0b65c431fbaa26517ee4379
Erin cooper <erinjacooper@gmail.com>
2018-10-11 10:01:57 +0100

    created web dev repo
    created www folder and index.html

--- ConverterProgram/converterProgram.py
+++ ConverterProgram/converterProgram.py
@@ -21,20 +21,46 @@ def userInput(message):
     userResponse = input(message)
     return userResponse
 
+                                    
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+#     INPUT SANITATION 
+
+
+#-----BINARY-SANITATION--------------------------------
+def binarySanitised():
+    byte = input("Please enter an 8-bit binary value:")
+    if len(byte) > 8:
+        print("Error: input longer than 8 digits")
+        binarySanitised()
+    elif len(byte) < 8:
+        print("Error: input longer than 8 digits")
+        binarySanitised()
+#-----HEXADECIMAL-SANITATION---------------------------
+def hexSanitised():
+    pass
+    
+    
+    
+    
+
+    
+
 #--------------------------------------------------------------------------------------------------------------------------
 #      BINARY TO DENARY
 
 
 def b2d():
     byte = input("Please enter a binary value:")
-    counter = len(byte)  #sets the counter to the length of the byte integer
-    denary = 0 #sets denary to 0
-    for bit in byte: #iterate over each bit in the byte
-        if bit == "1": #if the current bit = 1 do:
-            denary +=  2**counter  #setting denary to 2^the counter working out the current bits value
-        counter -= 1  #counter = counter - 1
+    counter = len(byte)                             # sets the counter to the length of the byte integer
+    denary = 0                                      # sets denary to 0
+    for bit in byte:                                # iterate over each bit in the byte
+        if bit == "1":                              # if the current bit = 1 do:
+            denary +=  2**counter                   # setting denary to 2^the counter working out the current bits value
+        counter -= 1                                # counter = counter - 1
     denary = math.floor(denary / 2)
-    return denary  #returns the denary string
+    return denary                                   # returns the denary string
 
 
 #--------------------------------------------------------------------------------------------------------------------------
@@ -43,13 +69,13 @@ def b2d():
 
 def d2b():
     denNum = input("Please enter a denary number up to 255")
-    Binary = ''                 #creating a blank output string
-    product = int(denNum)     #setting the "product" of the loop to the same as the input so it can iterate over it
-    while len(Binary) < 8:    #while loop to run until the binary output is 8 digits long
-        currentBin = product % 2   #sets the current binary digit to the remainder of prod / 2
-        product = math.floor(product / 2)  #dividing prod by 2, rounding down and setting it to itself
-        Binary = str(currentBin) + Binary   #concatenate the current binary number onto the front of the binary output
-    return Binary    #returns the binary string
+    Binary = ''                                     # creating a blank output string
+    product = int(denNum)                           # setting the "product" of the loop to the same as the input so it can iterate over it
+    while len(Binary) < 8:                          # while loop to run until the binary output is 8 digits long
+        currentBin = product % 2                    # sets the current binary digit to the remainder of prod / 2
+        product = math.floor(product / 2)           # dividing prod by 2, rounding down and setting it to itself
+        Binary = str(currentBin) + Binary           # concatenate the current binary number onto the front of the binary output
+    return Binary                                   # returns the binary string
 
 
 #--------------------------------------------------------------------------------------------------------------------------
@@ -60,12 +86,12 @@ def h2d():
     hexNum = input("Please enter a hex number up to 255")
     counter = 0
     denary = 0
-    for digit in reversed(hexNum):   #iterates through hexNum backwards
+    for digit in reversed(hexNum):                  # iterates through hexNum backwards
         try:
             digit = digit.upper()
         except:
             pass
-        if digit == "A":   #converts letters on hex to decimal values
+        if digit == "A":                            # converts letters on hex to decimal values
             digit = 10
         elif digit == "B":
             digit = 11
@@ -77,10 +103,10 @@ def h2d():
             digit = 14
         elif digit == "F":
             digit = 15
-        currentDen = int(digit) * 16**counter  #carries out the operation <digit> * 16^<counter> to calculate the decimal value of <digit>
-        counter += 1  #adds 1 to counter
-        denary += currentDen #adds the current denary to the final denary output
-    return denary  #returns the denary output
+        currentDen = int(digit) * 16**counter       # carries out the operation <digit> * 16^<counter> to calculate the decimal value of <digit>
+        counter += 1                                # adds 1 to counter
+        denary += currentDen                        # adds the current denary to the final denary output
+    return denary                                   # returns the denary output
 
 
 #--------------------------------------------------------------------------------------------------------------------------
@@ -91,9 +117,9 @@ def d2h():
     denNum = input("Please enter a denary number")
     hexa = ''
     prod = int(denNum)
-    while prod > 0:  #loop to run while the value of prod is greater than 0 (run until divisions all return 0)
-        remainder = prod % 16  #getting the remainder of the current value when divided by 16
-        if remainder == 10:  #converting decimal hex values to letters
+    while prod > 0:                                 # loop to run while the value of prod is greater than 0 (run until divisions all return 0)
+        remainder = prod % 16                       # getting the remainder of the current value when divided by 16
+        if remainder == 10:                         # converting decimal hex values to letters
             remainder = "A"
         elif remainder == 11:
             remainder = "B"
@@ -105,8 +131,8 @@ def d2h():
             remainder = "E"
         elif remainder == 15:
             remainder = "F"
-        prod = math.floor(prod / 16)  #dividing prod (current value) by 16 ready for next iteration
-        hexa = str(remainder) + hexa  #concatenating the remainder of the current calculation to the output variable
+        prod = math.floor(prod / 16)                # dividing prod (current value) by 16 ready for next iteration
+        hexa = str(remainder) + hexa                # concatenating the remainder of the current calculation to the output variable
     return hexa
 
 
@@ -116,18 +142,18 @@ def d2h():
 
 def b2h():
     byte = input("Please enter a binary value:")
-    counter = len(byte)  #sets the counter to the length of the byte integer
-    denary = 0 #sets denary to 0
-    for bit in byte: #iterate over each bit in the byte
-        if bit == "1": #if the current bit = 1 do:
-            denary +=  2**counter  #setting denary to 2^the counter working out the current bits value
-        counter -= 1  #counter = counter - 1
+    counter = len(byte)                             # sets the counter to the length of the byte integer
+    denary = 0                                      # sets denary to 0
+    for bit in byte:                                # iterate over each bit in the byte
+        if bit == "1":                              # if the current bit = 1 do:
+            denary +=  2**counter                   # setting denary to 2^the counter working out the current bits value
+        counter -= 1                                # counter = counter - 1
     denary = math.floor(denary / 2)
     hexa = ''
     prod = int(denary)
-    while prod > 0:  #loop to run while the value of prod is greater than 0 (run until divisions all return 0)
-        remainder = prod % 16  #getting the remainder of the current value when divided by 16
-        if remainder == 10:  #converting decimal hex values to letters
+    while prod > 0:                                 # loop to run while the value of prod is greater than 0 (run until divisions all return 0)
+        remainder = prod % 16                       # getting the remainder of the current value when divided by 16
+        if remainder == 10:                         # converting decimal hex values to letters
             remainder = "A"
         elif remainder == 11:
             remainder = "B"
@@ -139,8 +165,8 @@ def b2h():
             remainder = "E"
         elif remainder == 15:
             remainder = "F"
-        prod = math.floor(prod / 16)  #dividing prod (current value) by 16 ready for next iteration
-        hexa = str(remainder) + hexa  #concatenating the remainder of the current calculation to the output variable
+        prod = math.floor(prod / 16)                # dividing prod (current value) by 16 ready for next iteration
+        hexa = str(remainder) + hexa                # concatenating the remainder of the current calculation to the output variable
     return hexa
 
 
@@ -152,12 +178,12 @@ def h2b():
     hexNum = input("Please enter a hex number up to 255")
     counter = 0
     denary = 0
-    for digit in reversed(hexNum):  #iterates through hexNum backwards
+    for digit in reversed(hexNum):                  # iterates through hexNum backwards
         try:
             digit = digit.upper()
         except:
             pass
-        if digit == "A":   #converts letters on hex to decimal values
+        if digit == "A":                            # converts letters on hex to decimal values
             digit = 10
         elif digit == "B":
             digit = 11
@@ -169,16 +195,16 @@ def h2b():
             digit = 14
         elif digit == "F":
             digit = 15
-        currentDen = int(digit) * 16**counter  #carries out the operation <digit> * 16^<counter> to calculate the decimal value of <digit>
-        counter += 1  #adds 1 to counter
-        denary += currentDen #adds the current denary to the final denary output
-    Binary = ''                 #creating a blank output string
-    prod = int(denary)     #setting the "product" of the loop to the same as the input so it can iterate over it
-    while len(Binary) < 8:    #while loop to run until the binary output is 8 digits long
-        currentBin = prod % 2   #sets the current binary digit to the remainder of prod / 2
-        prod = math.floor(prod / 2)  #dividing prod by 2, rounding down and setting it to itself
-        Binary = str(currentBin) + Binary   #concatenate the current binary number onto the front of the binary output
-    return Binary    #returns the binary string
+        currentDen = int(digit) * 16**counter       # carries out the operation <digit> * 16^<counter> to calculate the decimal value of <digit>
+        counter += 1                                # adds 1 to counter
+        denary += currentDen                        # adds the current denary to the final denary output
+    Binary = ''                                     # creating a blank output string
+    prod = int(denary)                              # setting the "product" of the loop to the same as the input so it can iterate over it
+    while len(Binary) < 8:                          # while loop to run until the binary output is 8 digits long
+        currentBin = prod % 2                       # sets the current binary digit to the remainder of prod / 2
+        prod = math.floor(prod / 2)                 # dividing prod by 2, rounding down and setting it to itself
+        Binary = str(currentBin) + Binary           # concatenate the current binary number onto the front of the binary output
+    return Binary                                   # returns the binary string
 
 
 #--------------------------------------------------------------------------------------------------------------------------    
@@ -206,28 +232,27 @@ def c2d():
 def d2c():
     denNum = input("Please enter a denary number up to 127 and down to -128")
     Binary = ''
-    nFlag = 0
     if denNum[0] == "-":
         nFlag = 1
-        denNum = denNum.lstrip('-')
-        print("denNum:",denNum)
-        print("nFlag:",nFlag)               #creating a blank output string
-    product = int(denNum)     #setting the "product" of the loop to the same as the input so it can iterate over it
-    while len(Binary) < 7:    #while loop to run until the binary output is 8 digits long
-        currentBin = product % 2   #sets the current binary digit to the remainder of prod / 2
-        product = math.floor(product / 2)  #dividing prod by 2, rounding down and setting it to itself
-        Binary = str(currentBin) + Binary   #concatenate the current binary number onto the front of the binary output
-    Binary = str(nFlag) + Binary   
-    return Binary    #returns the binary string
-    
-        
+        denNum = denNum.lstrip('-')                 # removing the - from the denary string
+        denNum = 256 - int(denNum)                  # sets denary to 256 - denary so that the denary then works up from -128 rather than 0
+    else:
+        denNum = int(denNum)                        # sets the denary to the input with no change
+    while len(Binary) < 7:                          # while loop to run until the binary output is 7 digits long
+        currentBin = denNum % 2                     # sets the current binary digit to the remainder of prod / 2
+        denNum = math.floor(denNum / 2)             # dividing prod by 2, rounding down and setting it to itself
+        Binary = str(currentBin) + Binary           # concatenate the current binary number onto the front of the binary output
+    Binary = str(nFlag) + Binary                    # adds the nflag digit to the start of the binary
+    return Binary                                   # returns the binary string
 
 
+#--------------------------------------------------------------------------------------------------------------------------   
 
     
 selection = menu()
 
 if selection == "1":
+    binarySanitised()
     response = b2d()
     print(response)
 elif selection == "2":
